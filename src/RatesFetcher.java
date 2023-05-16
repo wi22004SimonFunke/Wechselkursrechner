1afeccc615eff4f80e7cde844d497e3e936685c6
s.funke <s.funke@hm-netzwerke.de>
2023-05-16 16:23:15 +0200

    Fetcher adapted to EUR, USD, GBP

--- src/RatesFetcher.java
+++ src/RatesFetcher.java
@@ -2,40 +2,56 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import org.json.JSONObject;
 
 public class RatesFetcher {
     public static void main(String[] args) {
-        String endpoint = "https://api.exchangerate-api.com/v4/latest/USD";
+        String[] currencies = {"GBP", "USD", "EUR"};
 
-        try {
-            // Create a URL object with the API endpoint
-            URL url = new URL(endpoint);
+        for (String baseCurrency : currencies) {
+            for (String resultCurrency : currencies) {
+                if (!baseCurrency.equals(resultCurrency)) {
+                    String endpoint = "https://api.exchangerate-api.com/v4/latest/" + baseCurrency;
 
-            // Create an HttpURLConnection object and open a connection
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.setRequestMethod("GET");
+                    try {
+                        // Create a URL object with the API endpoint
+                        URL url = new URL(endpoint);
 
-            // Get the response code
-            int responseCode = connection.getResponseCode();
+                        // Create an HttpURLConnection object and open a connection
+                        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                        connection.setRequestMethod("GET");
 
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Read the response using a BufferedReader
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                StringBuilder response = new StringBuilder();
-                String line;
+                        // Get the response code
+                        int responseCode = connection.getResponseCode();
 
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
+                        if (responseCode == HttpURLConnection.HTTP_OK) {
+                            // Read the response using a BufferedReader
+                            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                            StringBuilder response = new StringBuilder();
+                            String line;
+
+                            while ((line = reader.readLine()) != null) {
+                                response.append(line);
+                            }
+                            reader.close();
+
+                            // Parse the JSON response
+                            JSONObject jsonResponse = new JSONObject(response.toString());
+                            JSONObject rates = jsonResponse.getJSONObject("rates");
+
+                            // Get the exchange rate
+                            double exchangeRate = rates.getDouble(resultCurrency);
+
+                            // Print the exchange rate
+                            System.out.println("1 " + baseCurrency + " = " + exchangeRate + " " + resultCurrency);
+                        } else {
+                            System.out.println("Error: " + responseCode);
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
-                reader.close();
-
-                // Print the JSON response
-                System.out.println(response.toString());
-            } else {
-                System.out.println("Error: " + responseCode);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
